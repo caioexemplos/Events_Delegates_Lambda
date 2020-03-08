65ca20bda15cffb42883869cb12ac8f60d9a5dd0
DevCaioSchool <caio.treinamentos@gmail.com>
2020-03-08 13:45:20 -0300

    First Commit

---
+++ Aula3/ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,74 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+        private readonly Agencia _agencia;
+
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+
+            _agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+            AtualizarCamposDeTexto();
+            AtualizarControles();
+        }
+
+        private void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtTelefone.Text = _agencia.Telefone;
+            txtEndereco.Text = _agencia.Endereco;
+            txtDescricao.Text = _agencia.Descricao;
+        }
+
+        private void AtualizarControles()
+        {
+            RoutedEventHandler dialogResultTrue = (o, e) => DialogResult = true;
+            RoutedEventHandler dialogResultFalse = (o, e) => DialogResult = false;
+
+            var okEventHandler = dialogResultTrue + Fechar;
+            var cancelarEventHandler = dialogResultFalse + Fechar;
+
+            btnOk.Click += okEventHandler;
+            btnCancelar.Click += cancelarEventHandler;
+
+            txtNome.TextChanged += ValidarCampoNulo;
+            txtDescricao.TextChanged += ValidarCampoNulo;
+            txtEndereco.TextChanged += ValidarCampoNulo;
+            txtNumero.TextChanged += ValidarCampoNulo;
+            txtTelefone.TextChanged += ValidarCampoNulo;
+        }
+        
+        private void ValidarCampoNulo(object sender, EventArgs e)
+        {
+            var txt = sender as TextBox;
+            var textoEstaVazio = String.IsNullOrEmpty(txt.Text);
+
+            txt.Background = textoEstaVazio
+                ? new SolidColorBrush(Colors.OrangeRed)
+                : new SolidColorBrush(Colors.White);
+        }
+
+        private void Fechar(object sender, EventArgs e) =>
+            Close();
+    }
+}
